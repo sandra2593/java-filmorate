8ae139c95d026737327dffe29f93a55976c1591f
azaytseva <azaytseva@ozon.ru>
2023-04-02 18:26:07 +0100

    check_style

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Collection;
 
 @RestController
 public class UserController {
-    private final String QUERY_PATH = "/users";
+
     private final UserStorage userStorage;
     private final FriendsStorage friendsStorage;
 
@@ -23,34 +23,44 @@ public class UserController {
         this.userStorage = userStorage;
     }
 
-    @GetMapping(QUERY_PATH)
+    @GetMapping("/users")
     public Collection<User> findAll() {
         return userStorage.findAll();
     }
 
-    @PostMapping(value = QUERY_PATH)
+    @PostMapping(value = "/users")
     public User create(@Valid @RequestBody User user) {
        return userStorage.create(user);
     }
 
-    @PutMapping(value = QUERY_PATH)
+    @PutMapping(value = "/users")
     public User update(@Valid @RequestBody User user) {
         return userStorage.update(user);
     }
 
     @PutMapping("/users/{id}/friends/{friendId}")
-    public void addFriend(@PathVariable int id, @PathVariable int friendId) { friendsStorage.addFriend(id, friendId); }
+    public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        friendsStorage.addFriend(id, friendId);
+    }
 
     @DeleteMapping("/users/{id}/friends/{friendId}")
-    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) { friendsStorage.deleteFriend(id, friendId); }
+    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        friendsStorage.deleteFriend(id, friendId);
+    }
 
     @GetMapping("/users/{id}/friends")
-    public Collection<User> getFriends(@PathVariable int id) { return friendsStorage.getFriends(id); }
+    public Collection<User> getFriends(@PathVariable int id) {
+        return friendsStorage.getFriends(id);
+    }
 
     @GetMapping("/users/{id}/friends/common/{otherId}")
-    public Collection<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) { return friendsStorage.getCommonFriends(id, otherId);}
+    public Collection<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+        return friendsStorage.getCommonFriends(id, otherId);
+    }
 
     @GetMapping("/users/{id}")
-    public User getUserId(@PathVariable int id) { return userStorage.getUserId(id); }
+    public User getUserId(@PathVariable int id) {
+        return userStorage.getUserId(id);
+    }
 
 }
